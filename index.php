2d3c39dbd93d54967b08d73c1cda46efe9fe576e
Mészáros Mihály <misi@niif.hu>
2016-12-01 15:01:45 +0100

    change webm to mp4 and fix link change

--- index.php
+++ index.php
@@ -263,11 +263,11 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
                 <h1 style="visibility: visible; animation-name: flipInX;" class="cursive wow flipInX">STUN/TURN federation pilot</h1>
                 <h4>A federated joint effort to build STUN/TURN infrastructure for ICE (Interactive Connectivity Establishment) agents.</h4>
                 <hr>
-                <a href="#video-background" id="toggleVideo" data-toggle="collapse" class="btn btn-primary btn-xl">Toggle Video</a> &nbsp; <a href="#one" class="btn btn-primary btn-xl page-scroll">Get Started</a>
+                <a href="#video-background" id="toggleVideo" data-toggle="collapse" class="btn btn-primary btn-xl">Toggle Video</a> &nbsp; <a href="#intro" class="btn btn-primary btn-xl page-scroll">Get Started</a>
             </div>
         </div>
         <video style="visibility: visible; animation-delay: 0.5s; animation-name: fadeIn;" autoplay="autoplay" loop="" class="fillWidth fadeIn wow collapse in" data-wow-delay="0.5s" poster="img/bg.jpg" id="video-background">
-            <source src="/video/bg.webm" type="video/mp4">Your browser does not support the video tag. I suggest you upgrade your browser.
+            <source src="/video/bg.mp4" type="video/mp4">Your browser does not support the video tag. I suggest you upgrade your browser.
         </video>
     </header>
     <section class="bg-primary" id="intro">
